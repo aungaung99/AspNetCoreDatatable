7662cce9ae81afcdec1c1c991c5e5a722aac9838
Aung Naing Oo <57518163+aungaung99@users.noreply.github.com>
2023-02-22 01:17:51 +0630

    working with dynamic where

--- Controllers/DatatablesController.cs
+++ Controllers/DatatablesController.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace AspNetCoreDatatable.Controllers
@@ -105,6 +106,55 @@ namespace AspNetCoreDatatable.Controllers
             }
         }
 
+        [HttpPost("column-searching")]
+        public async Task<IActionResult> PostColumnSearchingAsync()
+        {
+            try
+            {
+                bool Searching = false;
+                List<Expression<Func<Street, bool>>> expressions = new();
+                System.Reflection.PropertyInfo[] props = typeof(Street).GetProperties();
+                for (int i = 0; i < props.Length; i++)
+                {
+                    string colName = Request.Form["columns[" + i + "][name]"].FirstOrDefault();
+                    string searchValue = Request.Form["columns[" + i + "][search][value]"].FirstOrDefault();
+                    if (!string.IsNullOrEmpty(searchValue))
+                    {
+                        Expression<Func<Street, bool>> expression = s => typeof(Street).GetProperty(colName).GetValue(s).ToString().Contains(searchValue);
+                        expressions.Add(expression);
+                        Searching = true;
+                    }
+                }
+
+                string draw = Request.Form["draw"].FirstOrDefault();
+                string start = Request.Form["start"].FirstOrDefault();
+                string length = Request.Form["length"].FirstOrDefault();
+                int pageSize = length != null ? Convert.ToInt32(length) : 0;
+                int skip = start != null ? Convert.ToInt32(start) : 0;
+                int recordsTotal = 0;
+
+                List<Street> streets = new();
+                if (Searching)
+                {
+                    //streets = await _context.Streets.Where(CombineWithOr(expressions.ToArray())).ToListAsync();
+                    streets = await _context.Streets.Where(s => typeof(Street).GetProperty("StreetName").GetValue(s).ToString().Contains(Request.Form["search[value]"].FirstOrDefault())).ToListAsync();
+                }
+                else
+                {
+                    streets = await _context.Streets.ToListAsync();
+                }
+                recordsTotal = streets.Count();
+                List<Street> data = pageSize < 0 ? streets : streets.Skip(skip).Take(pageSize).ToList();
+                var jsonData = new { draw, recordsFiltered = recordsTotal, recordsTotal, data };
+                return Ok(jsonData);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+                throw;
+            }
+        }
+
         [HttpPost("ordering")]
         public async Task<IActionResult> PostOrderingAsync()
         {
@@ -148,10 +198,43 @@ namespace AspNetCoreDatatable.Controllers
                 var jsonData = new { draw, recordsFiltered = recordsTotal, recordsTotal, data };
                 return Ok(jsonData);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 return BadRequest();
                 throw;
+            }
+        }
+
+        public static Expression<Func<T, bool>> CombineWithOr<T>(params Expression<Func<T, bool>>[] filters)
+        {
+            Expression<Func<T, bool>> first = filters.First();
+            ParameterExpression param = first.Parameters.First();
+            Expression body = first.Body;
+
+            foreach (Expression<Func<T, bool>> other in filters.Skip(1))
+            {
+                ReplaceParameter replacer = new ReplaceParameter
+                {
+                    OriginalParameter = other.Parameters.First(),
+                    NewParameter = param
+                };
+                // We need to replace the original expression parameter with the result parameter
+                body = Expression.Or(body, replacer.Visit(other.Body));
+            }
+
+            return Expression.Lambda<Func<T, bool>>(
+                body,
+                param
+            );
+        }
+
+        private class ReplaceParameter : ExpressionVisitor
+        {
+            public Expression OriginalParameter { get; set; }
+            public Expression NewParameter { get; set; }
+            protected override Expression VisitParameter(ParameterExpression node)
+            {
+                return node == OriginalParameter ? NewParameter : base.VisitParameter(node);
             }
         }
     }
