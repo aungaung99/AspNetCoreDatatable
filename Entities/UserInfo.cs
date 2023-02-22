98b537b3b6c314050625d25ad56f798201fbcfad
Aung Naing Oo <57518163+aungaung99@users.noreply.github.com>
2023-02-22 23:53:09 +0630

    update db

---
+++ Entities/UserInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace AspNetCoreDatatable.Entities
+{
+    [Table("UserInfo")]
+    public partial class UserInfo
+    {
+        [Key]
+        [StringLength(256)]
+        public string UserId { get; set; }
+        public bool? IsActive { get; set; }
+        [StringLength(50)]
+        public string Balance { get; set; }
+        [StringLength(50)]
+        public string Credit { get; set; }
+        [StringLength(50)]
+        public string Wallet { get; set; }
+        [StringLength(100)]
+        public string Picture { get; set; }
+        public int? Age { get; set; }
+        [StringLength(100)]
+        public string EyeColor { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(50)]
+        public string Gender { get; set; }
+        [StringLength(50)]
+        public string Company { get; set; }
+        [StringLength(50)]
+        public string Email { get; set; }
+        [StringLength(50)]
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? Registered { get; set; }
+    }
+}
